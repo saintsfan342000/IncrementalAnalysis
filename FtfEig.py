558ea2713f9478dc546aab14a42c0436ba63f41e
Martin_5E_New <mfscales@gmail.com>
2018-05-22 13:14:10 -0500

    LEp can return components of U if last arg given is True

--- FtfEig.py
+++ FtfEig.py
@@ -6,7 +6,7 @@ def Eig2x2(a,b,c,d):
     '''
     return (a + d - sqrt(a**2 + 4*b*c - 2*a*d + d**2))/2, (a + d + sqrt(a**2 + 4*b*c - 2*a*d + d**2))/2
 
-def LEp(F00, F01, F10, F11):
+def LEp(F00, F01, F10, F11, rtnU=False):
     U00 = (sqrt(F00**2 + F01**2 + F10**2 + F11**2 - sqrt(((F01 + F10)**2 + (F00 - F11)**2)*
            ((F01 - F10)**2 + (F00 + F11)**2)))*(-F00**2 + F01**2 - F10**2 + F11**2 + 
         sqrt(((F01 + F10)**2 + (F00 - F11)**2)*((F01 - F10)**2 + (F00 + F11)**2))) + 
@@ -34,4 +34,10 @@ def LEp(F00, F01, F10, F11):
     eigU = Eig2x2(U00, U01, U01, U11)
     LE0, LE1 = [log(u) for u in eigU]
     
-    return ( 2/3 * ( LE0**2 + LE1**2 + (-LE0-LE1)**2 ) )**0.5
+    if rtnU != True:
+        return ( 2/3 * ( LE0**2 + LE1**2 + (-LE0-LE1)**2 ) )**0.5
+    else:
+        return ( ( 2/3 * ( LE0**2 + LE1**2 + (-LE0-LE1)**2 ) )**0.5,
+                U00, U01, U11
+                )
+
